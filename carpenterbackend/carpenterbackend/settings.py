fc01dd4fde8ca0fd9230dd664baaf806eccb85bc
Siva <siva.usha236@gmail.com>
2025-12-06 13:41:35 +0530

    Configured PostgreSQL for Render

--- carpenterbackend/carpenterbackend/settings.py
+++ carpenterbackend/carpenterbackend/settings.py
@@ -93,11 +93,7 @@ WSGI_APPLICATION = 'carpenterbackend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    'default': dj_database_url.parse(config('DATABASE_URL'))
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
 
 
